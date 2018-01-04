8231ca132018dbc2a19276448c95a858e261ed79
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2018-01-04 16:06:56 +0700

    selection page done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 $( document ).ready(() => {
-
+	$.get('/api/categories', (data) => { $( '#categories-wrapper' ).html( data.result.map((o) => ("<div class='ipd-categories float-left uppercase cursor-pointer'>" + o + "</div>")).join('') ); });
 });

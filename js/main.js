d239434eb44a0811490c7493df1e7997efa1a4bf
Mahdan Ahmad Fauzi Al-Hasan <mahdan.ahmad@yahoo.com>
2018-01-08 08:02:04 +0700

    init content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+let category	= '';
+
 $( document ).ready(() => {
-	$.get('/api/categories', (data) => { $( '#categories-wrapper' ).html( data.result.map((o) => ("<div class='ipd-categories float-left uppercase cursor-pointer'>" + o + "</div>")).join('') ); });
+	$.get('/api/categories', (data) => { $( '#categories-wrapper' ).html( data.result.map((o) => ("<div class='ipd-categories float-left uppercase cursor-pointer' onclick='changeCategory(\"" + o + "\")'>" + o + "</div>")).join('') ); });
+
 });
+
+function changeCategory(val) {
+	category	= val;
+	$( '#selection' ).slideUp();
+}
